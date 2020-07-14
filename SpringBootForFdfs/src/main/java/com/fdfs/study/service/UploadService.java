f0fde655b2789f8bae03172f44185c639807036a
Wenzx <53310640+Wenzx1993@users.noreply.github.com>
2020-07-14 23:46:40 +0800

    fdfs token check
    the url token check，for the url

--- SpringBootForFdfs/src/main/java/com/fdfs/study/service/UploadService.java
+++ SpringBootForFdfs/src/main/java/com/fdfs/study/service/UploadService.java
@@ -1,18 +1,31 @@
 package com.fdfs.study.service;
 
 import org.csource.common.MyException;
-import org.csource.fastdfs.ClientGlobal;
-import org.csource.fastdfs.StorageClient;
-import org.csource.fastdfs.TrackerClient;
-import org.csource.fastdfs.TrackerServer;
+import org.csource.fastdfs.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 
 @Service
+@PropertySource("classpath:fastdfs-client.properties")
 public class UploadService {
 
+   @Value("${http_secret_key}")
+   private String secretKey;
+
+   @Value("${fdfs.remote-url}")
+   private String remoteUrl;
+
+    /**
+     * 上传逻辑
+     * @param file
+     * @return
+     */
     public String upload(MultipartFile file)  {
         //获取文件名
         String filename = file.getOriginalFilename();
@@ -27,11 +40,31 @@ public class UploadService {
             StorageClient storageClient = buildClient();
             //返回结果，第一个为组名，第二个为fdfs磁盘地址
             String[] result = storageClient.upload_file(file.getBytes(), extName, null);
-            return new StringBuffer(result[0]).append("/").append(result[1]).toString();
-        } catch (IOException | MyException e) {
+//            String srcUrl = new StringBuffer(result[0]).append("/").append(result[1]).toString();
+            return tokenUrl(result[1]);
+        } catch (IOException | MyException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return "上传失败！";
+    }
+
+    /**
+     * 防盗链地址
+     * @param srcUrl
+     * @return
+     */
+    public String tokenUrl(String srcUrl) throws UnsupportedEncodingException, NoSuchAlgorithmException, MyException {
+        int ts = (int) (System.currentTimeMillis()/1000);
+        String token = ProtoCommon.getToken(srcUrl, ts, secretKey);
+        StringBuffer url = new StringBuffer(remoteUrl);
+        url.append("/")
+            .append(srcUrl)
+            .append("?")
+            .append("token=")
+            .append(token)
+            .append("&ts=")
+            .append(ts);
+        return url.toString();
     }
 
     /**
@@ -45,4 +78,6 @@ public class UploadService {
         StorageClient storageClient = new StorageClient(trackerServer);
         return storageClient;
     }
+
+
 }
